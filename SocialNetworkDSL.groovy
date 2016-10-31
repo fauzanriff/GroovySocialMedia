3cfb6b78a44b276710bc1880765902881482ab13
Ahmad N. F <fahmadnaufal@gmail.com>
2016-10-31 16:57:23 +0700

    add post DSL

--- SocialNetworkDSL.groovy
+++ SocialNetworkDSL.groovy
@@ -1,11 +1,11 @@
-class SocialNetwork {
+class SocialNetworkDSL {
 
     def dbConnector
 
     def comment(post) {
         [from: { who ->
             [content: { content -> 
-                def user = dbConnector.getUserFromId(who)
+                def user = dbConnector.getUser(who)
             }]
         }]
     }
@@ -22,6 +22,7 @@ class SocialNetwork {
                                     def date = Date.parse('M-d-yyyy', dateofbirth)
                                     def profile = new Profile(name, biodata, date, sex, location)
                                     def user = new User(1, username, password, email, profile)
+                                    
                                     def id = dbConnector.addUser(user)
                                     return id   // return > 0 if adding is successful, 0 if existing, -1 if database error
                                 }]
@@ -41,6 +42,20 @@ class SocialNetwork {
             } else {
                 return false
             }
+        }]
+    }
+    
+    def post(content) {
+        [by : { who -> 
+            [privacy : { privacy -> 
+                def createTime = new Date().format("yyyy-MM-dd hh:mm:ss")
+                def updateTime = createTime
+                def user = dbConnector.getUser(who)
+                def post = new Post(1, content, createTime, updateTime, user, privacy)
+
+                def result = dbConnector.addPost(post)
+                return result   // return > 0 if adding is successful, 0 if existing, -1 if database error
+            }]
         }]
     }
 
